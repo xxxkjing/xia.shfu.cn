f6904c27a0f3779a2b69683039b8e9ca2fb23470
MinusTen <xkjing@outlook.com>
2025-03-07 14:59:12 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,46 +1,50 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import remarkToc from 'remark-toc';
-import remarkCollapse from 'remark-collapse';
-import sitemap from '@astrojs/sitemap';
-import { SITE } from './src/config';
-import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  public: {
+    giscusRepoId: import.meta.env.PUBLIC_GISCUS_REPO_ID,
+    giscusCategoryId: import.meta.env.PUBLIC_GISCUS_CATEGORY_ID
+  },
   integrations: [
     tailwind({
-      applyBaseStyles: false
+      applyBaseStyles: false,
     }),
     react(),
-    sitemap()
+    sitemap({
+      filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
   ],
   markdown: {
     remarkPlugins: [
       remarkToc,
-      remarkReadingTime,
       [
         remarkCollapse,
         {
-          test: 'Table of contents'
-        }
-      ]
+          test: "Table of contents",
+        },
+      ],
     ],
     shikiConfig: {
-      themes: {
-        light: 'rose-pine-dawn',
-        dark: 'rose-pine-moon'
-      },
+      // For more themes, visit https://shiki.style/themes
+      themes: { light: "min-light", dark: "night-owl" },
       wrap: true,
-      transformers: []
-    }
+    },
   },
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js']
-    }
+      exclude: ["@resvg/resvg-js"],
+    },
   },
-  scopedStyleStrategy: 'where'
+  scopedStyleStrategy: "where",
+  experimental: {
+    contentLayer: true,
+  },
 });
