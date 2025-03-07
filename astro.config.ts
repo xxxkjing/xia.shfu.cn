b0f039e703ce94ddc0846a7be6ee414d658ac74d
MinusTen <xkjing@outlook.com>
2025-03-07 15:02:14 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
-import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import remarkToc from 'remark-toc';
+import remarkCollapse from 'remark-collapse';
+import sitemap from '@astrojs/sitemap';
+import { SITE } from './src/config';
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,36 +16,34 @@ export default defineConfig({
   },
   integrations: [
     tailwind({
-      applyBaseStyles: false,
+      applyBaseStyles: false
     }),
     react(),
-    sitemap({
-      filter: page => SITE.showArchives || !page.endsWith("/archives"),
-    }),
+    sitemap()
   ],
   markdown: {
     remarkPlugins: [
       remarkToc,
+      remarkReadingTime,
       [
         remarkCollapse,
         {
-          test: "Table of contents",
-        },
-      ],
+          test: 'Table of contents'
+        }
+      ]
     ],
     shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
-      themes: { light: "min-light", dark: "night-owl" },
+      themes: {
+        light: "min-light", dark: "night-owl"
+      },
       wrap: true,
-    },
+      transformers: []
+    }
   },
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ['@resvg/resvg-js']
+    }
   },
-  scopedStyleStrategy: "where",
-  experimental: {
-    contentLayer: true,
-  },
+  scopedStyleStrategy: 'where'
 });
